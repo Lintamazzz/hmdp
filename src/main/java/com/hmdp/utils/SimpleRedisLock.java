f80c1ce4f5a730f3ebe50da727d6fc91097e0a2d
Lintamazzz <lintama@foxmail.com>
2024-04-22 17:26:46 +0800

    用Lua脚本保证分布式锁解锁时多条命令的原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,14 +1,26 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        // 提前加载 lua 脚本
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     private String key;
     private StringRedisTemplate stringRedisTemplate;
 
@@ -33,13 +45,19 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        // 获取锁标识、线程标识
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + key);
-        long threadId = Thread.currentThread().getId();
-        // 判断标识是否一致
-        if ((ID_PREFIX + threadId).equals(id)) {
-            // 一致才释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + key);
-        }
+        // 调用 Lua 脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + key), ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unlock() {
+//        // 获取锁标识、线程标识
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + key);
+//        long threadId = Thread.currentThread().getId();
+//        // 判断标识是否一致
+//        if ((ID_PREFIX + threadId).equals(id)) {
+//            // 一致才释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + key);
+//        }
+//    }
 }
